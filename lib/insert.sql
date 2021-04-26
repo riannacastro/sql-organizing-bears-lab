ea0b3d47720dc7074f6a805453ef4c9a8b982324
Rianna <riannaxcastro@gmail.com>
2021-04-25 21:20:06 -1000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 4, "M", "Brown", "sweet", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 5, "M", "Black", "Loud", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 3, "F", "Gray", "Sassy", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 8, "M", "Black", "Agressive", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 1, "F", "White", "Playful", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 5, "M", "Green", "Grouchy", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 6, "F", "Light Brown", "Nice", true);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 4,"M", "Gray", "mean", true);
